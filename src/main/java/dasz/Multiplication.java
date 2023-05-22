af75ebd839c869de92016f0ab886524b3d8b0da9
dariuszszmycinski <68079317+dariuszszmycinski@users.noreply.github.com>
2023-05-22 09:45:19 +0200

    Add files via upload

---
+++ src/main/java/dasz/Multiplication.java
@@ -0,0 +1,12 @@
+package dasz;
+public class Multiplication{
+    public static int [][] multiplicationTable(int n){
+        int [][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j]=(i+1)*(j+1);
+            }
+        }
+        return result;
+    }
+}
